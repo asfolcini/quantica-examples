f30e7a9e90a362d0062199955e3725983d63ee18
Alberto Sfolcini <a.sfolcini@gmail.com>
2020-02-10 15:50:14 +0100

    Helloworld

--- README.md
+++ README.md
@@ -1,27 +1,14 @@
-<<<<<<< HEAD
-# Skeleton for Java/Maven/JUnit project
+# QUANTiCA examples coded with java
 
-A simple skeleton written in Java using Maven for the build and jUnit for tests.
+Collection of examples written in java. Feel free to clone and produce something more unique.
 
-* Java 8
-* Unit tests with [JUnit 5](https://junit.org/junit5/)
-* Integration tests with [JUnit 5](https://junit.org/junit5/)
-* Code coverage reports via [JaCoCo](https://www.jacoco.org/jacoco/)
-* A Maven build that puts it all together
+#### Requirements
 
-## Running the tests
+* Nats Server
+* quantica-core jar dependency
+* mariaDB server
+* metabase server
 
-* To run the unit tests, call `mvn test`
-* To run the integration tests as well, call `mvn verify`
-* Code coverage reports are generated when `mvn verify` (or a full `mvn clean install`) is called.
-  Point a browser at the output in `target/site/jacoco-both/index.html` to see the report.
 
-## Tests & Integration Tests
-
-* Write your test cases in `src/test/java/*Test.java` 
-* Write your Integration Test cases in `src/test/java/*IT.java`
-
-=======
-# quantica-examples
-QUANTiCA examples
->>>>>>> branch 'master' of https://github.com/asfolcini/quantica-examples.git
+#### Skeleton.Helloworld
+Basic example with BogusMarketDataFeed.
---
+++ src/main/java/skeleton/HelloWorld.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package skeleton;
+
+
+import quantica.broker.paper.PaperBroker;
+import quantica.engine.QuanticaEngine;
+import quantica.marketdatafeed.bogus.BogusMarketDataFeed;
+import quantica.model.event.CandleEvent;
+import quantica.model.marketdatafeed.types.MarketDataEventType;
+import quantica.model.security.types.Currency;
+import quantica.model.security.types.SecurityType;
+import quantica.model.strategy.IStrategy;
+import quantica.model.strategy.Strategy;
+import quantica.strategy.utils.LoadSecurities;
+
+
+/**
+ * HELLOWORLD SYSTEM
+ * @author alberto.sfolcini
+ */
+public class HelloWorld {
+
+
+	class HelloworldStrategy extends Strategy implements IStrategy{				
+		
+		public HelloworldStrategy() {
+			super.setStrategyName("Hello World!");
+		}
+		
+		@Override
+		public void onEvent(Object event) {
+			CandleEvent ce = (CandleEvent) event;									
+			log(ce.toString());			
+		}				
+	} // end of MySystem	
+	
+	
+	public HelloWorld() {							
+		
+		PaperBroker broker	= new PaperBroker("PAPER");		
+		broker.setBalance(1_000_000);
+		
+		// DATAFEEDS
+		BogusMarketDataFeed myDF = new BogusMarketDataFeed(500);		
+		myDF.setMarketDataEventType(MarketDataEventType.CANDLE_EVENT);
+		myDF.subscribeSecurities(new LoadSecurities(SecurityType.STK,"SMART",Currency.USD,"IBM,XOM,BHP").getSecurities());
+								
+		// QUANTICA ENGINE
+		QuanticaEngine engine = new QuanticaEngine();
+		engine.addBroker(broker);
+		engine.addDataFeed(myDF);
+		engine.addStrategy(new HelloworldStrategy());
+		engine.runForMillis(20_000);				
+		
+	}
+	
+	// start it up
+	public static void main(String[] args) {			
+		new HelloWorld();
+	}
+}
